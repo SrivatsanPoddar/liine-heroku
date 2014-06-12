a77a5596832119f69249daf780f5ba0164832175
Piyush Poddar <ppod1991@gmail.com>
2014-06-12 19:17:51 -0400

    Added dummy user

--- web.js
+++ web.js
@@ -8,6 +8,13 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+
+app.get('/users', function(req, res) {
+  res.send('Dummy User :(');
+});
+
+
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
