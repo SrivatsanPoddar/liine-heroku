c307086d4c7020d2c6c10bd7254d009969558eda
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-06-14 17:19:15 -0400

    Retrying with help of a tutorial

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var express = require("express");
+var express = require("express")
+var pg = require('pg').native,
+    connectionString = "postgres://ivaqkulwuyokvo:JBfCRSFIcaWoqRI_jE0dL36DnV@ec2-107-21-100-118.compute-1.amazonaws.com:5432/dbjkvhetm21oap",
+    start = new Date(),
+    port = process.env.PORT || 5000,
+    client;
 var logfmt = require("logfmt");
-var pg = require('pg');
 var app = express();
 
 app.use(logfmt.requestLogger());
+
+client = new pg.Client(connectionString);
+client.connect();
 
 app.get('/', function(req, res)
 {
@@ -19,12 +26,25 @@ app.get('/users', function(req, res)
 app.get('/akshay', function(req, res)
 {
     res.send('Akshay can finally use Node.js');
-})
+});
 
 app.get('/testquery', function(req, res)
 {
-    res.send("Eventually this will display SQL query");
-})
+    query = client.query('SELECT * WHERE PARENT_NODE = NULL FROM instructiontree');
+    query.on('row', function(result)
+    {
+        console.log(result);
+
+        if (!result)
+        {
+            return res.send('No data found');
+        }
+        else
+        {
+            res.send('Visits today: ' + result.count);
+        }
+    });
+});
 
 var port = Number(process.env.PORT || 5000);
 
