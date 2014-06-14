02c761f31dfe72fcd34fe67f2e873d2db4cebf79
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-06-14 15:50:46 -0400

    Akshay is testing

--- web.js
+++ web.js
@@ -4,18 +4,25 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res)
+{
   res.send('Hello World!');
 });
 
 
-app.get('/users', function(req, res) {
+app.get('/users', function(req, res)
+{
   res.send('Dummy User :(');
 });
 
-
+app.get('/akshay', function(req, res)
+{
+    res.send('Akshay can finally use Node.js');
+})
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
+
+app.listen(port, function()
+{
   console.log("Listening on " + port);
 });
