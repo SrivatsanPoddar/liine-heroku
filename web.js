a862f466589ee2b0c303e1352d28ae330368226d
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-06-15 17:01:15 -0400

    Ok, well that worked...

--- web.js
+++ web.js
@@ -30,7 +30,7 @@ app.get('/testquery', function(req, res)
         {
           return console.error('error fetching client from pool', err);
         }
-        client.query('SELECT * FROM instructiontree', function(err, result)
+        client.query('SELECT * FROM instructiontree WHERE parent_node_id IS NULL', function(err, result)
         {
             //call `done()` to release the client back to the pool
             done();
