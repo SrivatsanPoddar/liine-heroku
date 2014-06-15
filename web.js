555c29db3cea07741eb3306af87184db157cb25a
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-06-15 16:35:22 -0400

    Trying again to fix it

--- web.js
+++ web.js
@@ -21,31 +21,30 @@ app.get('/akshay', function(req, res)
 {
     res.send('Akshay can finally use Node.js');
 });
-/**
+
 app.get('/testquery', function(req, res)
 {
     pg.connect(conString, function(err, client, done)
     {
-      if(err)
-      {
-        return console.error('error fetching client from pool', err);
-      }
-      client.query('SELECT * WHERE PARENT_NODE = NULL FROM instructiontree', function(err, result)
-      {
-        //call `done()` to release the client back to the pool
-        done();
         if(err)
         {
-          return console.error('error running query', err);
+          return console.error('error fetching client from pool', err);
         }
-        console.log(result.rows[0].display_text);
-        console.log(result.rows[1].display_text);
-        res.send(result.rows[0].display_text);
-        res.send(result.rows[1].display_text);
-      });
+        client.query('SELECT * WHERE PARENT_NODE = NULL FROM instructiontree', function(err, result)
+        {
+            //call `done()` to release the client back to the pool
+            done();
+            if(err)
+            {
+              return console.error('error running query', err);
+            }
+            console.log(result.rows[0].display_text);
+            res.send(result.rows[0].display_text);
+
+        });
     });
 });
-*/
+
 var port = Number(process.env.PORT || 5000);
 
 app.listen(port, function()
