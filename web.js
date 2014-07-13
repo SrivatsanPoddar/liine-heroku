cd3d1b54d98175044bb4c8984b8a1e416146a197
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-07-13 12:20:00 -0400

    Cleaned

--- web.js
+++ web.js
@@ -38,32 +38,6 @@ app.get('/users', function(req, res)
   res.send('Dummy User :(');
 });
 
-app.get('/akshay', function(req, res)
-{
-    res.send('Akshay can finally use Node.js');
-});
-
-app.get('/testquery', function(req, res)
-{
-    pg.connect(conString, function(err, client, done)
-    {
-        if(err)
-        {
-          return console.error('error fetching client from pool', err);
-        }
-        client.query('SELECT * FROM instructiontree WHERE parent_node_id IS NULL', function(err, result)
-        {
-            //call `done()` to release the client back to the pool
-            done();
-            if(err)
-            {
-              return console.error('error running query', err);
-            }
-            res.send(result);
-        });
-    });
-});
-
 var port = Number(process.env.PORT || 5000);
 
 app.listen(port, function()
